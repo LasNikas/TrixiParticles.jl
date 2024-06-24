02079be486d8083ac1f162e817b27372b0f7e576
LasNikas <niklas.neher@web.de>
2024-06-24 13:55:16 +0200

    add example files

---
+++ examples/preprocessing/liquify_julia_logo.jl
@@ -0,0 +1,37 @@
+using TrixiParticles
+
+particle_spacing = 0.02
+fluid_density = 1000.0
+solid_density = 300.0
+
+dir = joinpath(expanduser("~/"), "Data", "stl-files", "julia_logo")
+
+file_names = [
+    "letters",
+    "dot_1",
+    "dot_2",
+    "dot_3",
+    "dot_4"
+]
+
+# Load shapes
+shapes = Vector{TrixiParticles.Shapes}()
+for file in file_names
+    push!(shapes, load_shape(joinpath(dir, file * ".stl")))
+end
+
+# Sample shapes
+initial_conditions = Vector{TrixiParticles.InitialCondition}()
+for i in eachindex(file_names)
+    density = file_names[i] == "letters" ? fluid_density : solid_density
+    point_in_shape_algorithm = WindingNumberJacobson(; shape=shapes[i],
+                                                     winding_number_factor=0.2,
+                                                     hierarchical_winding=true)
+
+    push!(initial_conditions,
+          ComplexShape(shapes[i]; particle_spacing, density, max_nparticles=1e7,
+                       point_in_shape_algorithm))
+
+    # write to vtk
+    trixi2vtk(initial_conditions[i], filename="initial_condition_" * file_names[i])
+end
