91f2c875e2e76af87f2aabdb4473ea9dfe5bd20e
LasNikas <niklas.nehe@web.de>
2025-09-10 22:24:36 +0200

    add shifting

--- src/general/semidiscretization.jl
+++ src/general/semidiscretization.jl
@@ -1042,6 +1042,7 @@ function set_system_links(system::OpenBoundarySPHSystem, semi)
                                  system.initial_condition,
                                  fluid_system, # link to fluid system
                                  system.fluid_system_index,
+                                 system.smoothing_kernel,
                                  system.smoothing_length,
                                  system.mass,
                                  system.volume,
@@ -1050,6 +1051,7 @@ function set_system_links(system::OpenBoundarySPHSystem, semi)
                                  system.boundary_zone_indices,
                                  system.boundary_zones,
                                  system.buffer,
+                                 system.pressure_acceleration_formulation,
                                  system.acceleration,
                                  system.cache)
 end
